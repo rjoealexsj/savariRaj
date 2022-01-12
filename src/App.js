6c8eb40a41f4b8c7283bc3ba51723d6d75bd8c06
Savari SJ <97437865+savarisj@users.noreply.github.com>
2022-01-12 15:59:34 +0530

    imguploadprocess

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
 import logo from './logo.svg';
 import './App.css';
+import imag1 from '../src/images/IMG.jpg'
 
 function App() {
   return (
-   <div></div>
+   <div>
+     <center>
+ <img src={imag1} 
+   style ={{width:'200px', borderRadius:'50%'}} />
+     </center>
+
+  
+   
+
+   </div>
+
   );
 }
 
