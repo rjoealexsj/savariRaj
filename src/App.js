b1b05b2c6df028f005c2ef07b37c00819886cc58
Savari SJ <97437865+savarisj@users.noreply.github.com>
2022-01-13 14:33:54 +0530

    addingsecondimage

--- src/App.js
+++ src/App.js
@@ -1,34 +1,35 @@
 import logo from './logo.svg';
 import './App.css';
 import imag1 from '../src/images/IMG1.jpg'
-
+import imag2 from '../src/images/IMG.jpg'
 function App() {
   return (
    <div>
      <div>
+       <h1>
        <center>
        MY GALLERY
        
        </center>
+       </h1>
+      
    
     <div>
   
     </div>
-
-
-       
      </div>
      <center>
  <img src={imag1} 
    style ={{width:'300px', borderRadius:'50%'}} />
      </center>
-
-  <center>
-
-    THANK YOU 
+     <img src={imag2} 
+   style ={{width:'300px', borderRadius:'50%'}} />
+<h5>
+<center>
+   THANK YOU 
   </center>
-   
-
+</h5>
+  
    </div>
 
   );
