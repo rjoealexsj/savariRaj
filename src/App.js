eab0506df86582732ce40cca0461cd87fac713cd
Savari SJ <97437865+savarisj@users.noreply.github.com>
2022-01-14 16:01:13 +0530

    vjresidence

--- src/App.js
+++ src/App.js
@@ -1,86 +1,20 @@
 import logo from './logo.svg';
 import './App.css';
-import './css/bootstrap.min.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import IDNavbar from './components/IDNavbar.js';
-import Home from './components/Home';
 
-import { Col, FormGroup, Input, Label, Row} from 'reactstrap';
+
+
+import Home from './components/Home';
 
 function App() {
   return (
   <div className='container' style={{paddingTop:'50px'}}>
-    <div className='text-center p-4 bg-primary'>
-      <h1 style={{color:'white'}}>Vidyajyoti Jesuit Residence </h1>     
-    </div>
-    <FormGroup style={{overflow:"visible", paddingTop:'50px'}}>
-    <Row style={{marginBottom:'20px'}}>
-    
-    <Col xs={3} lg={2}>
-    <Label for="usernameL">Name</Label>
-    </Col>
-    <Col xs={9} lg={4}>
-    <Input type="text" placeholder='Enter Your Name' value="" />
-    </Col>
-    <Col xs={3} lg={2}>
-    <Label for="usernameL">Password</Label>
-    </Col>
-    <Col xs={9} lg={4}>
-    <Input type="password" placeholder='Enter Your Password' value="" />
-    </Col>
+  <IDNavbar/>
 
-    </Row>
 
-    <Row style={{marginBottom:'20px'}}>
 
-    <Col xs={3} lg={2}>
-   <Label for="usernameL">Email</Label>
-   </Col>
-   <Col xs={9} lg={4}>
-   <Input type="text" placeholder='Enter Your Email' value="" />
-   </Col>
-   <Col xs={3} lg={2}>
-   <Label for="usernameL">Phone Number</Label>
-    </Col>
-    <Col xs={9} lg={4}>
-    <Input type="text" placeholder='Enter Your Phone Number' value="" />
-    </Col>>
-    </Row>
-
-<Row style={{marginBottom:'20px'}}>
-<Col xs={3} lg={2}>
-    <Label for="Province">Province</Label>
-    </Col>
-    <Col xs={9} lg={4}>
-    <Input type="text" placeholder='Enter Your Province' value="" />
-    </Col>
-    <Col xs={3} lg={2}>
-    <Label for="dob">Date of Birth'</Label>
-    </Col>
-    <Col xs={9} lg={4}>
-    <Input type="text" placeholder='Enter Your Date of Birth' value="" /> 
-      </Col>
-</Row>
-
-<Row style={{marginBottom:'20px'}}>
-<Col xs={3} lg={2}>
-    <Label for="Blood">Blood Group</Label>
-    </Col>
-    <Col xs={9} lg={4}>
-    <Input type="text" placeholder='Enter Your Blood Group' value="" />
-    </Col>
-    <Col xs={3} lg={2}>
-    <Label for="roomno">Room No'</Label>
-    </Col>
-    <Col xs={9} lg={4}>
-    <Input type="text" placeholder='Enter Your Room No' value="" /> 
-      </Col>
-</Row>
-
-</FormGroup>
-<div className='text-center'>
-<button className='btn' style={{border:'2px solid blue'}}>Submit</button>
-</div>
-
+   <Home />
 </div>
   );
 }
---
+++ src/components/IDNavbar.js
@@ -0,0 +1,67 @@
+import React, { Component } from 'react'
+import { NavbarBrand, NavbarToggler, Navbar, Collapse, Nav, NavItem, NavLink, UncontrolledDropdown, DropdownItem, DropdownToggle, DropdownMenu, NavbarText } from 'reactstrap'
+export default class IDNavbar extends Component {
+    render() {
+        return (
+            <div>
+               <div>
+  <Navbar
+    color="dark"
+    dark
+    expand="md"
+    fixed="top"
+    full
+  >
+    <NavbarBrand href="/">
+      reactstrap
+    </NavbarBrand>
+    <NavbarToggler onClick={function noRefCheck(){}} />
+    <Collapse navbar>
+      <Nav
+        className="me-auto"
+        navbar
+      >
+        <NavItem>
+          <NavLink href="/components/">
+            Components
+          </NavLink>
+        </NavItem>
+        <NavItem>
+          <NavLink href="https://github.com/reactstrap/reactstrap">
+            GitHub
+          </NavLink>
+        </NavItem>
+        <UncontrolledDropdown
+          inNavbar
+          nav
+        >
+          <DropdownToggle
+            caret
+            nav
+          >
+            Options
+          </DropdownToggle>
+          <DropdownMenu right>
+            <DropdownItem>
+              Option 1
+            </DropdownItem>
+            <DropdownItem>
+              Option 2
+            </DropdownItem>
+            <DropdownItem divider />
+            <DropdownItem>
+              Reset
+            </DropdownItem>
+          </DropdownMenu>
+        </UncontrolledDropdown>
+      </Nav>
+      <NavbarText>
+        Simple Text
+      </NavbarText>
+    </Collapse>
+  </Navbar>
+</div>
+            </div>
+        )
+    }
+}
